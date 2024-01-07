5ba4ce6d95c52f410767834296460a90718bd6a1
nandorfn <firnandorfn@gmail.com>
2024-01-07 11:51:46 +0700

    update: next config

--- app/helpers/mock-test.ts
+++ app/helpers/mock-test.ts
@@ -1,7 +1,7 @@
 import { TCarType } from "./types";
 
 export const carMock: TCarType = {
-  vehicle: 'Tesla Model S',
+  vehicle: 'Regular',
   price: 'IDR 200.000',
   imageURL: 'http://',
   description: ['7 seat']
